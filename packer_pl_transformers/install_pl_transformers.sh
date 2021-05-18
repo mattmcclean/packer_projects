ad7b39271f4fd85306f13d4803882cb1394216f2
Matt McClean <mmcclean@amazon.com>
2021-05-18 21:57:45 +0100

    added transfomer AMI

---
+++ packer_pl_transformers/install_pl_transformers.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# install supervisor
+sudo apt install supervisor -y
+
+# Install the Conda environment
+ENV_NAME=pl_transformers_p37
+conda create --yes -n ${ENV_NAME} python=3.7
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate ${ENV_NAME}
+pip install torch torchvision torchaudio
+pip install pytorch-lightning  
+pip install ray[default] ray[tune] ray[serve]
+pip install boto3
+pip install transformers
+pip install ipykernel
+pip install lightning-transformers["extra"]
+
+# install a ipykernel in enviornment
+python -m ipykernel install --user --name=${ENV_NAME}
+
+# setup tensorboard and mlflow
+conda activate base
+pip install mlflow
+pip install tensorboard
+
+# remove the efa conf file with ulimit set too low
+sudo rm /etc/security/limits.d/efa.conf
+
+# Install Prometheus
+echo "Installing Prometheus"
+wget -O $HOME/prometheus.tar.gz https://github.com/prometheus/prometheus/releases/download/v2.27.0/prometheus-2.27.0.linux-amd64.tar.gz
+sudo tar xvfz $HOME/prometheus.tar.gz -C /opt
+sudo ln -s /opt/prometheus-2.27.0.linux-amd64 /opt/prometheus
+rm $HOME/prometheus.tar.gz
+
+# Install Graphana
+echo "Installing Graphana"
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana -y
+
+echo 'export PATH="$HOME/anaconda3/envs/'${ENV_NAME}'/bin:$PATH"' >> ~/.bashrc
