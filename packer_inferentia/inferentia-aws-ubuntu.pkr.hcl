861ce6f260bb7322b4c7caef6209ade063f1e236
Matt McClean <mmcclean@amazon.com>
2022-06-03 16:15:55 +0100

    added inferentia project

---
+++ packer_inferentia/inferentia-aws-ubuntu.pkr.hcl
@@ -0,0 +1,37 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = ">= 0.0.1"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+source "amazon-ebs" "ubuntu" {
+  ami_name      = "deep-learning-inferentia-ubuntu-{{isotime \"2006-01-02\"}}"
+  instance_type = "t3.xlarge"
+  region        = "eu-west-1"
+  source_ami_filter {
+    filters = {
+      name                = "Deep Learning AMI (Ubuntu 18.04) Version *"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    most_recent = true
+    owners      = ["898082745236"]
+  }
+  ssh_username = "ubuntu"
+}
+
+build {
+  sources = [
+    "source.amazon-ebs.ubuntu"
+  ]
+
+  provisioner "shell" {
+    scripts = [
+      "install_inferentia.sh"
+    ]
+    pause_before = "600s"
+  }
+}
---
+++ packer_inferentia/install_inferentia.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+sudo apt-get update -y
+
+# install neuron drivers
+sudo apt --fix-broken install -y
+sudo apt-get install linux-headers-$(uname -r) -y
+sudo apt-get install aws-neuron-dkms --allow-change-held-packages -y
+sudo apt-get install aws-neuron-tools -y
+
+# install OCI add hooks
+cd /tmp
+wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
+tar xzf go1.18.3.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+export PATH=$PATH:/usr/local/go/bin
+rm /tmp/go1.18.3.linux-amd64.tar.gz
+
+#go install github.com/joeshaw/json-lossless@latest
+git clone https://github.com/awslabs/oci-add-hooks /tmp/oci-add-hooks
+cd /tmp/oci-add-hooks
+make build
+sudo cp /tmp/oci-add-hooks/oci-add-hooks /usr/local/bin/
+
+# restart docker
+sudo cp /opt/aws/neuron/share/docker-daemon.json /etc/docker/daemon.json
+sudo service docker restart
+
+# install ECS agent
+curl -O https://s3.eu-west-1.amazonaws.com/amazon-ecs-agent-eu-west-1/amazon-ecs-init-latest.amd64.deb
+sudo dpkg -i amazon-ecs-init-latest.amd64.deb
